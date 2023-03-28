72743a546bb489915545c747126a3f77f7cf2f51
GabrielaGP2023 <cursos@gabrielagomezpuente.es>
2023-03-28 21:14:56 +0200

    Proyecto a las 21:00

---
+++ src/com/hedima/presentacion/OperadoresLogicos.java
@@ -0,0 +1,38 @@
+package com.hedima.presentacion;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        //Operadores de Comparacion
+        //Devuelven un valor booleano.
+        int a = 10;
+        int b = 20;
+        boolean resultado = a >= b;
+        System.out.println(resultado);
+        resultado = a <= b;
+        System.out.println(resultado);
+
+        int c = 10;
+        int d = 10;
+        resultado = c == d;
+        System.out.println(resultado);
+        resultado = c!= d;
+        System.out.println(resultado);
+
+        //Operadores Lógicos
+        //and &&
+        //or ||
+        //not!
+        a=20;
+        b=10;
+        c=30;
+        resultado = (a > b) && (a < c);
+        System.out.println("and->"+resultado);
+        resultado = (a > b) || (a < c);
+        System.out.println(resultado);
+        resultado =!(a > b);
+        System.out.println(resultado);
+
+
+
+    }
+}
